660751b82e345a85f124c934df46e5597592cf2c
SuhudAlyeqeenDotGithub <alyekeenlearningzone@outlook.com>
2025-04-27 18:19:33 +0100

    Added button opacity options

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-themeButton text-themeButtonText shadow-xs hover:bg-themeText-80",
+        default: "bg-themeButton text-themeButtonText shadow-xs hover:bg-themeButton-90",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
